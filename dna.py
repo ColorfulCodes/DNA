6dfde8b47b63fb0dfa4c6be71cf620f10d9bfca2
Colorful Codes <purplewater772@gmail.com>
2016-08-01 18:24:19 -0400

    Add files via upload
    Three suspects were taken in for questioning and swabs of their DNA, taken. It is up to us to determine which suspect's DNA matches with that found at the crime scene.

---
+++ dna.py
@@ -0,0 +1,36 @@
+sample = ['GTA','GGG','CAC']
+
+def read_dna(dna_file):
+    dna_data = ""
+    with open(dna_file, "r") as f:
+        for line in f:
+            dna_data += line
+    return dna_data
+  
+def dna_codons(dna):
+    codons = []
+    for i in range(0,len(dna),3):
+        if (i+3) < len(dna):
+            codons.append(dna[i:i+3])
+    return codons
+
+  
+def match_dna(dna):
+    matches = 0
+    for codon in dna:
+        if codon in sample:
+            matches += 1
+    return matches
+    
+def is_criminal(dna_sample):
+    dna_data = read_dna(dna_sample)
+    codons = dna_codons(dna_data)
+    num_matches = match_dna(codons)
+    if num_matches >= 3:
+        print "The number of codon matches is %s, the investigation should proceed." % num_matches
+    else:
+        print "The number of matches is %s, the suspect should be let go." % num_matches
+        
+is_criminal('suspect1.txt')
+is_criminal('suspect2.txt')
+is_criminal('suspect3.txt')
